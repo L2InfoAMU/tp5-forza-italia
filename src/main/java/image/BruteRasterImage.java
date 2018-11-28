327b38aaecb5e36119e9e201fddeec50cb9bc3e9
a17006822 <iskylalz@gmail.com>
2018-11-28 11:51:49 +0100

    Correction du constructeur BruteRasterImage et implémentation des méthodes setWidth, setHeight, createRepresentation et les 3 méthodes setPixelsColor.

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -8,15 +8,19 @@ import util.Matrices;
 public class BruteRasterImage implements Image {
 
     private Color[][] colors;
-    private Color color;
     private int width;
     private int height;
 
     public BruteRasterImage(Color color, int width, int height){
 
-        this.color=color;
         this.width=width;
         this.height=height;
+        Color colors[][] = new Color[width][height];
+        for(int widthMatrice=0; widthMatrice<width; widthMatrice++){
+            for(int heightMatrice=0; heightMatrice<height; heightMatrice++){
+                this.colors[widthMatrice][heightMatrice]=color;
+            }
+        }
     }
 
     public BruteRasterImage(Color[][] colors){
@@ -32,8 +36,38 @@ public class BruteRasterImage implements Image {
         return height;
     }
 
+    protected void setWidth(int width){
+        this.width=width;
+    }
+
+    protected void setHeight(int height){
+        this.height=height;
+    }
+
     public Color getPixelColor(int x, int y){
         return colors[x][y];
     }
+
+    public void createRepresentation(){
+        Color colors[][] = new Color[width][height];
+    }
+
+    public void setPixelColor(Color color, int x, int y){
+        colors[x][y]=color;
+    }
+
+    private void setPixelsColor(Color[][] pixels){
+        this.colors = pixels;                                   // à vérifier si il ne faut pas itérer pour chaque pixel
+    }
+
+    private void setPixelsColor(Color color){
+        for(int widthMatrice=0; widthMatrice<getWidth(); widthMatrice++){
+            for(int heightMatrice=0; heightMatrice<getHeight(); heightMatrice++){
+                setPixelColor(color,heightMatrice,widthMatrice);
+            }
+        }
+
+    }
+
 
 }
