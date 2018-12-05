5e67420ff27068ac732c9a3f6c78e8a281ede72c
a17006822 <iskylalz@gmail.com>
2018-12-05 13:11:05 +0100

    Implémentation de la classe Rectangle avec ses attributs et son constructeur.

---
+++ src/main/java/image/Rectangle.java
@@ -0,0 +1,22 @@
+package image;
+
+import java.awt.*;
+import javafx.scene.paint.Color;
+
+public class Rectangle implements Shape {
+
+    public int x;
+    public int y;
+    public int width;
+    public int height;
+    public Color color;
+
+    public Rectangle(int x,int y, int width, int height, Color color){
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+        this.color=color;
+    }
+    
+}
