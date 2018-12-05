701ef695cc64a543ba089e094d3a0332a733c8c3
a17006822 <iskylalz@gmail.com>
2018-12-05 13:12:01 +0100

    Implémentation de la méthode contains dans la classe Rectangle.

--- src/main/java/image/Rectangle.java
+++ src/main/java/image/Rectangle.java
@@ -7,8 +7,8 @@ public class Rectangle implements Shape {
 
     public int x;
     public int y;
-    public int width;
-    public int height;
+    private int width;
+    private int height;
     public Color color;
 
     public Rectangle(int x,int y, int width, int height, Color color){
@@ -18,5 +18,8 @@ public class Rectangle implements Shape {
         this.height=height;
         this.color=color;
     }
-    
+
+    public boolean contains(Point point){
+        return point.x<=x+width && point.y<=y+height && point.x>=x && point.y>=y;
+    }
 }
